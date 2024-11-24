6428f26831e9e9a038c5b2856af436c423cab9c0
Enea <gegaenea@yahoo.com>
2024-11-24 19:35:44 +0300

    api key

--- lesson-booker-main/frontend/src/firebase/firebase.js
+++ lesson-booker-main/frontend/src/firebase/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+console.log(process.env.FIREBASE_API_KEY)
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDXVHuFXazlkc1Hm6-Ivw_Qs8804_3YeBk",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "lesson-booker.firebaseapp.com",
   projectId: "lesson-booker",
   storageBucket: "lesson-booker.firebasestorage.app",
   messagingSenderId: "919093612309",
-  appId: "1:919093612309:web:fd8a20a8c991ef713a231c",
+  appId: process.env.FIREBASE_appID,
   measurementId: "G-VBSH6R8N61"
 };
 
